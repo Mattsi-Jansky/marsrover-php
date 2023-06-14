3322288998628223c3e03352b1476509fb28f921
Mattsi <mattsi.jansky@codurance.com>
2023-06-14 09:26:33 +0100

    Refactor tests to be parameterised

--- tests/RoverTest.php
+++ tests/RoverTest.php
@@ -1,35 +1,23 @@
 <?php
 
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 use rover\Rover;
 use function PHPUnit\Framework\assertEquals;
 
 class RoverTest extends TestCase
 {
-    public function testGivenNoInstructionsDoesNothing()
+    #[Test]
+    #[TestWith(["L","0:0:W"])]
+    #[TestWith(["R","0:0:E"])]
+    #[TestWith(["","0:0:N"])]
+    public function given_commands_output_new_position($commands, $expectedOutput)
     {
         $rover = new Rover();
 
-        $result = $rover->execute("");
+        $result = $rover->execute($commands);
 
-        assertEquals("0:0:N", $result);
-    }
-
-    public function testGivenRotateRightCommandFacesEast()
-    {
-        $rover = new Rover();
-
-        $result = $rover->execute("R");
-
-        assertEquals("0:0:E", $result);
-    }
-
-    public function testGivenRotateLeftCommandFacesWest()
-    {
-        $rover = new Rover();
-
-        $result = $rover->execute("L");
-
-        assertEquals("0:0:W", $result);
+        assertEquals($expectedOutput, $result);
     }
 }
